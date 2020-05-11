71066c4f4b2605959c311dd9c07efeb8850c85c2
sanhae11 <simwoo2641@gmail.com>
2020-05-12 02:43:02 +0900

    Todo - create_todo 버튼 생성, 버튼 누르면 해당 activity로 감

--- app/src/main/java/com/example/mp_plancat/TodoFragment.java
+++ app/src/main/java/com/example/mp_plancat/TodoFragment.java
@@ -1,5 +1,6 @@
 package com.example.mp_plancat;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,11 +8,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class TodoFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_todo, container, false);
+
+        Button btn_create_todo = (Button) rootView.findViewById(R.id.btn_create_todo);
+        btn_create_todo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().startActivity(new Intent(getActivity(), CreateTodoActivity.class));
+            }
+        });
         return rootView;
     }
 }

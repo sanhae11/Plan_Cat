6145d4d4c813aa1e8110f7c50975125df8ac66ac
sanhae11 <simwoo2641@gmail.com>
2020-05-12 06:15:59 +0900

    Edit Todo와 Create Todo 화면에서 뒤로 가기 버튼 동작

--- app/src/main/java/com/example/mp_plancat/CreateTodoActivity.java
+++ app/src/main/java/com/example/mp_plancat/CreateTodoActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class CreateTodoActivity extends AppCompatActivity {
 
@@ -24,5 +25,16 @@ public class CreateTodoActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_todo_actionbar, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ //toolbar의 back키 눌렀을 때 동작
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

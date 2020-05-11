4f03db07bdb4bd96ba382396877927470a2e60f2
sanhae11 <simwoo2641@gmail.com>
2020-05-12 06:10:54 +0900

    Edit Todo와 Create Todo 화면에 action bar 적용

--- app/src/main/java/com/example/mp_plancat/CreateTodoActivity.java
+++ app/src/main/java/com/example/mp_plancat/CreateTodoActivity.java
@@ -1,8 +1,11 @@
 package com.example.mp_plancat;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class CreateTodoActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class CreateTodoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_todo);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.show();
+        actionBar.setTitle("Create Todo");
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_todo_actionbar, menu);
+        return true;
     }
 }

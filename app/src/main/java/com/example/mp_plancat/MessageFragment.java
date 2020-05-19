56990dc88cf0a2e87a61ad90689b52c00c291cd6
leeyeji <leeyeji99>
2020-05-20 03:55:15 +0900

    포인트 보상 팝업창 현재 날짜 출력하도록 수정

---
+++ app/src/main/java/com/example/mp_plancat/MessageFragment.java
@@ -0,0 +1,51 @@
+package com.example.mp_plancat;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class MessageFragment extends DialogFragment implements View.OnClickListener {
+
+    public static final String TAG_EVENT_DIALOG = "dialog_event";
+    TextView txt_date;
+    private SimpleDateFormat mformat = new SimpleDateFormat("yyyy년 M월 d일 "); // 날짜 포맷
+    Button btn_confirm;
+
+    public MessageFragment(){}
+
+    public static MessageFragment getInstance(){
+        MessageFragment e = new MessageFragment();
+        return e;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        View v = inflater.inflate(R.layout.popup_silvercoin, container);
+
+        txt_date = (TextView)v.findViewById(R.id.txt_date);
+        Date date = new Date();
+        String str = txt_date.getText().toString();
+        String time = mformat.format(date);
+        time = time + str;
+        txt_date.setText(time); //현재 날짜로 설정
+
+        btn_confirm = (Button)v.findViewById(R.id.btn_confirm);
+        btn_confirm.setOnClickListener(this);
+        return v;
+    }
+
+    @Override
+    public void onClick(View v){
+        dismiss();
+    }
+}

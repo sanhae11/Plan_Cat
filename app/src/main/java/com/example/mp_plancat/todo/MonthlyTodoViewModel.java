a8a9ae46a57a970cb2e70ced04011b6d16764eb0
sanhae11 <simwoo2641@gmail.com>
2020-06-19 08:29:19 +0900

    MonthlyFragment : recyclerview 출력

---
+++ app/src/main/java/com/example/mp_plancat/todo/MonthlyTodoViewModel.java
@@ -0,0 +1,40 @@
+package com.example.mp_plancat.todo;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.mp_plancat.database.entity.Todo;
+import com.example.mp_plancat.todo.weekly.WeeklyTodoRepository;
+
+import java.util.List;
+
+public class MonthlyTodoViewModel extends AndroidViewModel {
+    private MonthlyTodoRepository repository;
+    private LiveData<List<Todo>> allTodos;
+
+    public MonthlyTodoViewModel(@NonNull Application application){
+        super(application);
+        repository = new MonthlyTodoRepository(application);
+        allTodos = repository.getAllTodos();
+    }
+
+    public void insert(Todo todo){
+        repository.insert(todo);
+    }
+
+    public void update(Todo todo){
+        repository.update(todo);
+    }
+    public void delete(Todo todo){
+        repository.delete(todo);
+    }
+    public void deleteAllTodos(){
+        repository.deleteAllTodos();
+    }
+    public LiveData<List<Todo>> getAllTodos(){
+        return allTodos;
+    }
+}

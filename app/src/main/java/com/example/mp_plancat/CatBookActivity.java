d6c6a7495761bdb394e7f83b9413c337be42b5cf
sanhae11 <simwoo2641@gmail.com>
2020-06-26 18:46:03 +0900

    shop, catbook 화면의 액션바 없앰

--- app/src/main/java/com/example/mp_plancat/CatBookActivity.java
+++ app/src/main/java/com/example/mp_plancat/CatBookActivity.java
@@ -1,5 +1,6 @@
 package com.example.mp_plancat;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,6 +15,9 @@ public class CatBookActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cat_book);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
 
         exit_button=(ImageButton) findViewById(R.id.exitbtn);
 

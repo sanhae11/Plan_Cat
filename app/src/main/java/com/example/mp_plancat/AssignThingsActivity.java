b128e8e3762ca8e440c0074aff98a0e7831cabf3
leeyeji <leeyeji99>
2020-06-29 01:16:46 +0900

    shop scroll view + recyclerview 구현

--- app/src/main/java/com/example/mp_plancat/AssignThingsActivity.java
+++ app/src/main/java/com/example/mp_plancat/AssignThingsActivity.java
@@ -3,23 +3,41 @@ package com.example.mp_plancat;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class AssignThingsActivity extends AppCompatActivity {
+
+    Button btn_assign1;
+    CatFragment fragmentCat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assign_things);
 
+        fragmentCat = new CatFragment();
+
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true); //뒤로 가기 버튼
         actionBar.show();
         actionBar.setTitle("");
+
+        btn_assign1 = (Button) findViewById(R.id.btn_assign1);
+        btn_assign1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+            }
+        });
     }
 
     public boolean onCreateOptionsMenu(Menu menu){

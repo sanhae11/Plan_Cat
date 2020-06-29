0b724689d223655ea3f3427fb5c9add8e25ddad7
sanhae11 <simwoo2641@gmail.com>
2020-06-30 02:48:00 +0900

    AssignThingsActivity : 투명 버튼 누르면 캣메인화면으로 감

--- app/src/main/java/com/example/mp_plancat/AssignThingsActivity.java
+++ app/src/main/java/com/example/mp_plancat/AssignThingsActivity.java
@@ -14,11 +14,15 @@ import android.widget.ImageView;
 
 public class AssignThingsActivity extends AppCompatActivity {
     private ImageView location1, location2, location3, location4, location5;
+    private ImageView x_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assign_things);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
 
         location1 = (ImageView) findViewById(R.id.assign_location_1);
         location2 = (ImageView) findViewById(R.id.assign_location_2);
@@ -29,11 +33,48 @@ public class AssignThingsActivity extends AppCompatActivity {
         location1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                AssignThingsDialog assignThingsDialog = new AssignThingsDialog();
+                assignThingsDialog.show(getSupportFragmentManager(), "Assign Things Dialog");
+            }
+        });
+        location2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AssignThingsDialog assignThingsDialog = new AssignThingsDialog();
+                assignThingsDialog.show(getSupportFragmentManager(), "Assign Things Dialog");
+            }
+        });
+        location3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AssignThingsDialog assignThingsDialog = new AssignThingsDialog();
+                assignThingsDialog.show(getSupportFragmentManager(), "Assign Things Dialog");
+            }
+        });
+        location4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AssignThingsDialog assignThingsDialog = new AssignThingsDialog();
+                assignThingsDialog.show(getSupportFragmentManager(), "Assign Things Dialog");
+            }
+        });
+        location5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AssignThingsDialog assignThingsDialog = new AssignThingsDialog();
+                assignThingsDialog.show(getSupportFragmentManager(), "Assign Things Dialog");
             }
         });
 
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
+        x_btn = (ImageView) findViewById(R.id.x_btn);
+        x_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //엑스 버튼 누를 시 액티비티 종료
+                finish();
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/mp_plancat/AssignThingsDialog.java
+++ app/src/main/java/com/example/mp_plancat/AssignThingsDialog.java
@@ -9,8 +9,11 @@ import android.view.View;
 import android.widget.ImageButton;
 
 import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class AssignThingsDialog extends DialogFragment {
+    private static final String TAG1 = "Cat Fragment";
     private ImageButton btn_check_white, btn_x_white;
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -29,7 +32,13 @@ public class AssignThingsDialog extends DialogFragment {
             public void onClick(View v) {
                 //Todo 장난감+고양이 이미지 메인화면에 뿌리기
                 //Todo 프래그먼트 시작할 때 어떻게 해야 함,,?
-                startActivity(new Intent(getActivity(), CatFragment.class));
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+
+                Bundle bundle = new Bundle();
+                bundle.putInt("AssignActivity", 1);
+                intent.putExtra("AssignActivity", bundle);
+                startActivity(intent);
+
                 dismiss();
             }
         });

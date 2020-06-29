b3b7131ef37af98bb706304a778f996162b85ed8
sanhae11 <simwoo2641@gmail.com>
2020-06-30 01:31:37 +0900

    객체 선언 imageview로 수정

--- app/src/main/java/com/example/mp_plancat/AssignThingsActivity.java
+++ app/src/main/java/com/example/mp_plancat/AssignThingsActivity.java
@@ -10,20 +10,21 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class AssignThingsActivity extends AppCompatActivity {
-    private ImageButton location1, location2, location3, location4, location5;
+    private ImageView location1, location2, location3, location4, location5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assign_things);
 
-        location1 = (ImageButton) findViewById(R.id.assign_location_1);
-        location2 = (ImageButton) findViewById(R.id.assign_location_2);
-        location3 = (ImageButton) findViewById(R.id.assign_location_3);
-        location4 = (ImageButton) findViewById(R.id.assign_location_4);
-        location5 = (ImageButton) findViewById(R.id.assign_location_5);
+        location1 = (ImageView) findViewById(R.id.assign_location_1);
+        location2 = (ImageView) findViewById(R.id.assign_location_2);
+        location3 = (ImageView) findViewById(R.id.assign_location_3);
+        location4 = (ImageView) findViewById(R.id.assign_location_4);
+        location5 = (ImageView) findViewById(R.id.assign_location_5);
 
         location1.setOnClickListener(new View.OnClickListener() {
             @Override
